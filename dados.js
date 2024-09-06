9b2718079a5e33fcc7d364aa43168e35cf92cf77
Carla De Campos Oliveira <carlaoliveira.sp@gmail.com>
2024-09-06 19:12:29 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Letty Sitter",
+        descricao: "A Lettysitter se destaca como um hotel especializado em cuidar de pets não convencionais, oferecendo um ambiente seguro e acolhedor para animais exóticos e silvestres. Com profissionais qualificados e apaixonados por animais, a Lettysitter garante o bem-estar e a saúde de cada hóspede, proporcionando os melhores cuidados durante a ausência dos seus tutores. ",
+        link: "https://www.instagram.com/lettysitter/",
+        tags: "pet exotico silvestre"
+    },
+    {
+        titulo: "Débora Catsitter",
+        descricao: "A Débora é uma petsitter especializada em gatos que oferece um serviço personalizado e atencioso para cada felino. Ela entende que cada gato tem suas próprias necessidades e rotina, por isso adapta seus cuidados de acordo com as especificações de cada cliente. Seja para gatos mais independentes ou mais sociáveis, a Débora garante que seu amigo felino esteja sempre bem cuidado e feliz.",
+        link: "https://www.instagram.com/debora_petsitter/",
+        tags: "cat gato felino"
+    },
+    {
+        titulo: "Hotelzinho tia Gi",
+        descricao: "No Hotelzinho da Tia Gi, os cães podem aproveitar uma variedade de atividades, como passeios, brincadeiras e muito carinho. Além disso, a hospedagem inclui alimentação balanceada, higiene e acompanhamento veterinário, garantindo o bem-estar dos pets. Com um espaço amplo e seguro, o Hotelzinho da Tia Gi é o lugar perfeito para seu cão se divertir e relaxar.",
+        link: "https://www.instagram.com/hotelzinhodatiagi/",
+        tags: "cachorro dog doguinho"
+    }
+];
